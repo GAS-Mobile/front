1c7318f77013e69a90d7d74fc8a4f198004e1bc1
Igor <isidoroigor9@gmail.com>
2023-08-24 11:36:23 -0300

    Confirmação de senha no cadastro

--- app/signup.js
+++ app/signup.js
@@ -1,41 +1,44 @@
-import React, { useState } from 'react';
-import { Stack } from 'expo-router';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import React, { useState } from 'react'
+import { Stack } from 'expo-router'
+import { View, Text, TouchableOpacity, Alert } from 'react-native'
 import { TextInput } from 'react-native-paper'
 import { styles } from '../styles/authStyles'
 import { validateEmail, validateCpf } from '../utils/validators'
 import { EmailInput, PasswordInput } from '../components/authInputs'
 
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
-  const [viewPassword, setViewPassword] = useState(false);
+  const [viewPassword, setViewPassword] = useState(false)
+  const [viewPasswordConfirmation, setViewPasswordConfirmation] = useState(false)
 
   const [errors , setErrors] = useState({
     name: false,
     cpf: false,
     email: false,
-    password: false
+    password: false,
+    passwordConfirmation: false,
   })
 
   const handleCpfChange = (text) => {
-    const numericOnly = text.replace(/\D/g, '');
-    let formattedCpf = '';
+    const numericOnly = text.replace(/\D/g, '')
+    let formattedCpf = ''
   
     if (numericOnly.length <= 3) {
-      formattedCpf = numericOnly;
+      formattedCpf = numericOnly
     } else if (numericOnly.length <= 6) {
-      formattedCpf = numericOnly.slice(0, 3) + '.' + numericOnly.slice(3);
+      formattedCpf = numericOnly.slice(0, 3) + '.' + numericOnly.slice(3)
     } else if (numericOnly.length <= 9) {
       formattedCpf =
         numericOnly.slice(0, 3) +
         '.' +
         numericOnly.slice(3, 6) +
         '.' +
-        numericOnly.slice(6);
+        numericOnly.slice(6)
     } else {
       formattedCpf =
         numericOnly.slice(0, 3) +
@@ -44,7 +47,7 @@ const Signup = () => {
         '.' +
         numericOnly.slice(6, 9) +
         '-' +
-        numericOnly.slice(9, 11);
+        numericOnly.slice(9, 11)
     }
   
     setCpf(formattedCpf)
@@ -55,15 +58,17 @@ const Signup = () => {
       name: false,
       cpf: false,
       email: false,
-      password: false
+      password: false,
+      passwordConfirmation: false,
     })
 
-    if (!name || !cpf || !email || !password) {
+    if (!name || !cpf || !email || !password || !passwordConfirmation) {
       setErrors({
         name: !name,
         cpf: !cpf,
         email: !email,
         password: !password,
+        passwordConfirmation: !passwordConfirmation,
       })
 
       Alert.alert('Para realizar o cadastro é necessário preencher todos os campos.')
@@ -78,8 +83,8 @@ const Signup = () => {
         }
       })
 
-      Alert.alert('Favor inserir um CPF válido (11 digitos, apenas números).');
-      return;
+      Alert.alert('Favor inserir um CPF válido (11 digitos, apenas números).')
+      return
     }    
   
     if (!validateEmail(email)) {
@@ -90,24 +95,33 @@ const Signup = () => {
         }
       })
 
-      Alert.alert('Favor inserir um email válido.');
+      Alert.alert('Favor inserir um email válido.')
       return
     }
 
-    Alert.alert('Cadastro realizado com sucesso!');
-    console.log('Name:', name);
-    console.log('CPF:', cpf);
-    console.log('Email:', email);
-    console.log('Password:', password);
+    if (password !== passwordConfirmation) {
+      setErrors(prevState => {
+        return {
+          ...prevState,
+          passwordConfirmation: true,
+        }
+      })
+      Alert.alert('A confirmação de senha não confere.')
+      return
+    }
 
-    setEmail('');
-    setName('');
-    setCpf('');
-    setPassword('');
-  };
+    Alert.alert('Cadastro realizado com sucesso!')
+    console.log('Name:', name)
+    console.log('CPF:', cpf)
+    console.log('Email:', email)
+    console.log('Password:', password)
+    console.log('PasswordConfirmation:', passwordConfirmation)
 
-  const toggleViewPassword = () => {
-    setViewPassword(!viewPassword);
+    setEmail('')
+    setName('')
+    setCpf('')
+    setPassword('')
+    setPasswordConfirmation('')
   }
 
   return (
@@ -163,6 +177,14 @@ const Signup = () => {
           passwordError={errors.password}
         />
 
+        <PasswordInput 
+          label='Confirmar senha'
+          password={passwordConfirmation}
+          setPassword={setPasswordConfirmation}
+          viewPassword={viewPasswordConfirmation}
+          setViewPassword={setViewPasswordConfirmation}
+          passwordError={errors.passwordConfirmation}
+        />
       </View>
 
       <TouchableOpacity style={styles.button} onPress={handleSignup}>
@@ -174,7 +196,7 @@ const Signup = () => {
       </TouchableOpacity>
 
     </View>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
